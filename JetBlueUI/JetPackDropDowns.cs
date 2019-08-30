3622c4d24e381b8f2807027514aaa381ca9959b0
Oleg Brutich <oleg87arh@gmail.com>
2019-08-30 22:08:46 +0300

    Added abstract class JetPackDropDownBase

--- JetBlueUI/JetPackDropDowns.cs
+++ JetBlueUI/JetPackDropDowns.cs
@@ -12,37 +12,77 @@ using RevitServices.Persistence;
 
 namespace JetBlueUI
 {
+    public abstract class JetPackDropDownBase : DSDropDownBase
+    {
+        protected JetPackDropDownBase(string value) : base(value) { }
+
+
+        [JsonConstructor]
+        public JetPackDropDownBase(string value, IEnumerable<PortModel> inPorts, IEnumerable<PortModel> outPorts) : base(value, inPorts, outPorts) { }
+
+
+        /// <summary>
+        /// Whether it have valid Enumeration values to the output.
+        /// </summary>
+        /// <param name="itemValueToIgnore"></param>
+        /// <param name="selectedValueToIgnore"></param>
+        /// <returns>True is that there are valid values to output,false is that only a null value to output.</returns>
+        public Boolean CanBuildOutputAst(string itemValueToIgnore = null, string selectedValueToIgnore = null)
+        {
+            if (Items.Count == 0 || SelectedIndex < 0)
+                return false;
+            if (!string.IsNullOrEmpty(itemValueToIgnore) && Items[0].Name == itemValueToIgnore)
+                return false;
+            if (!string.IsNullOrEmpty(selectedValueToIgnore) && Items[SelectedIndex].Name == selectedValueToIgnore)
+                return false;
+            return true;
+        }
+    }
+
+
     [NodeName("Parameter Filter Elements")]
     [NodeDescription("Represents collection of Parameter Filter elements")]
     [NodeCategory("JetPack")]
     [IsDesignScriptCompatible]
-    public class FiltersByRule : DSDropDownBase
+    public class FiltersByRule : JetPackDropDownBase
     {
-        public FiltersByRule() : base("Parameter Filter Element") { }
+        private const string NO_PARAMETER_FILTERS = "No parameter filters available.";
+        private const string outputName = "Parameter Filter Element";
+
+        public FiltersByRule() : base(outputName) { }
 
 
         [JsonConstructor]
-        public FiltersByRule(IEnumerable<PortModel> inPorts, IEnumerable<PortModel> outPorts) : base("Parameter Filter Element", inPorts, outPorts) { }
+        public FiltersByRule(IEnumerable<PortModel> inPorts, IEnumerable<PortModel> outPorts) : base(outputName, inPorts, outPorts) { }
 
         protected override SelectionState PopulateItemsCore(string currentSelection)
         {
-
             Items.Clear();
 
             var fec = new FilteredElementCollector(DocumentManager.Instance.CurrentDBDocument);
+
             fec.OfClass(typeof(ParameterFilterElement));
             var elements = fec.ToElements();
+            if (!elements.Any())
+            {
+                Items.Add(new DynamoDropDownItem(NO_PARAMETER_FILTERS, null));
+                SelectedIndex = 0;
+                return SelectionState.Done;
+            }
 
-            List<DynamoDropDownItem> newItems = elements.Select(x => new DynamoDropDownItem(x.Name, x.Name)).ToList();
+            foreach (var element in elements)
+            {
+                Items.Add(new DynamoDropDownItem(element.Name, element.Name));
+            }
 
-            Items.AddRange(newItems);
-
+            Items.OrderBy(x => x.Name).ToObservableCollection();
             return SelectionState.Done;
         }
         
+
         public override IEnumerable<AssociativeNode> BuildOutputAst(List<AssociativeNode> inputAstNodes)
         {
-            if(Items.Count == 0 || SelectedIndex < 0)
+            if (!CanBuildOutputAst(NO_PARAMETER_FILTERS))
             {
                 return new[] { AstFactory.BuildAssignment(GetAstIdentifierForOutputIndex(0), AstFactory.BuildNullNode()) };
             }
